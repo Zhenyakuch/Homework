4e75ab05da72abb2ff51c8aa453922eb9ad004d3
Zhenyakuch <zhenya_247@mail.ru>
2019-09-04 10:31:56 +0300

    DZ-7

---
+++ HM-7/src/Rectangle.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * имеется прямоугольное отверстие размерами a и b, определить, можно ли его полностью закрыть
+ * круглой картонкой радиусом r.
+ */
+public class Rectangle {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input a:  ");
+        double a = scan.nextDouble();
+
+        System.out.print("Input b: ");
+        double b = scan.nextDouble();
+
+        double square_rect = a * b;
+        System.out.println("Square rectangle = "  + square_rect);
+
+        System.out.print("Input radius: ");
+        double radius = scan.nextDouble();
+        double pi = Math.PI;
+        double square_circle = pi*Math.pow(radius,2);
+        System.out.println("Square circle = " + square_circle);
+
+        if (square_circle > square_rect){
+            System.out.println("can close");
+        }else {
+            System.out.println("can't close");
+        }
+
+
+    }
+}
