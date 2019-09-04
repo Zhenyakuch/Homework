0fb035e82fa3d66caa7cab777abdfa63dcfd59c6
Zhenyakuch <zhenya_247@mail.ru>
2019-09-04 10:00:51 +0300

    DZ -6

---
+++ HM-6/src/NumberCheck.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class NumberCheck
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int number = scan.nextInt();
+
+        int length = String.valueOf(number).length();
+        System.out.println("Length number: " + length);
+
+        if (length == 3) {
+            System.out.println("the number is a three digit");
+        } else {
+            System.out.println("the number is not a three digit");
+            System.out.println("length number = " + length);
+        }
+
+
+        int lastnumber = number % 10;
+        if (lastnumber == 7) {
+            System.out.println("The last number = 7");
+        } else {
+            System.out.println("The last number not = 7");
+            System.out.println("The last number = " + lastnumber);
+        }
+
+        int even = number % 2;
+        if (even == 0) {
+            System.out.println(number + " - even number");
+        } else {
+            System.out.println(number + " - not even number");
+        }
+    }
+}
