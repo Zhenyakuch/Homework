6667650ad3d7e57b90838064ca60a809225710c7
Zhenyakuch <zhenya_247@mail.ru>
2019-09-05 09:30:49 +0300

    DZ -16

---
+++ HM-16/src/SumBetween.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+/*
+Определите сумму элементов одномерного массива, расположенных между
+минимальным и максимальным значениями.
+*/
+public class SumBetween {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int len = random.nextInt(5) + 10;
+
+        int[] marks = new int[len];
+        for (int i = 0; i <marks.length ; i++) {
+            marks[i] = random.nextInt(6) + 4;
+            System.out.print(" " + marks[i]);
+        }
+        System.out.println();
+        int max = 0;
+        int min =  10;
+        int index_max = 0;
+        int index_min = 0;
+
+        for(int i = 0; i < marks.length; i++){
+            if(max < marks[i]){
+                max = marks[i];
+                index_max = i;
+                // System.out.println();
+            }
+
+            if(min > marks[i]){
+                min = marks[i];
+                index_min = i;
+                // System.out.println();
+            }
+        }
+
+int sum = 0;
+        if(index_min < index_max){
+            for (int i = index_min; i < index_max -1; i++) {
+                sum += marks[i+1];
+
+            } System.out.println("sum = "+sum);
+        }else{
+            for (int i = index_max; i < index_min - 1; i++) {
+                sum += marks[i+1];
+
+            }System.out.println("sum = " + sum);
+        }
+
+        System.out.println("max - " + max +  "  index = " + index_max);
+        System.out.println("min - " + min+  "  index = " + index_min);
+    }
+}
