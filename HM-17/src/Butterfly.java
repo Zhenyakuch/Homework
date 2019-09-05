ed2956ddbbcfc05e0a3efaf4fa1b4f34c97ad16e
Zhenyakuch <zhenya_247@mail.ru>
2019-09-05 09:56:02 +0300

    DZ-17

---
+++ HM-17/src/Butterfly.java
@@ -0,0 +1,37 @@
+/*
+Создать двухмерный квадратный массив, и заполнить его «бабочкой»
+*/
+
+public class Butterfly {
+    public static void main(String[] args) {
+
+        int but[][]=new int[5][5];
+        int i,j;
+
+
+        for (i=0;i<but.length/2;i++){
+            for (j=0;j<but[i].length;j++){
+                if ((j<i) || (j>=(but[i].length-i)))
+                    but[i][j]=0;
+                else
+                    but[i][j]=1;
+
+            }
+
+        }
+        for (i=but.length-1; i>=but.length/2; i--){
+            for (j=0; j<but[i].length; j++){
+                if ((j<(but[i].length-1-i)) || (j>i))
+                    but[i][j]=0;
+                else
+                    but[i][j]=1;}
+        }
+        for (i=0;i<but.length;i++){
+            for(j=0;j<but[i].length;j++){
+                System.out.print(but[i][j]);
+            }
+            System.out.println(" ");
+        }
+    }
+    }
+
