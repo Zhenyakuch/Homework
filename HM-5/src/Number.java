43573da8713f73cedd5081da849c33966471fb81
Zhenyakuch <zhenya_247@mail.ru>
2019-09-09 11:08:22 +0300

    revision of the project

--- HM-5/src/Number.java
+++ HM-5/src/Number.java
@@ -9,15 +9,10 @@ public class Number {
         System.out.println("input number: ");
         int number = input.nextInt();
 
-      //  for (int i = 0; i<4; i++){
             int number1 = number%10;
             int number2 = number/10%10;
             int number3 = number/100%10;
             int number4 = number/1000%10;
-            System.out.println(number1);
-            System.out.println(number2);
-            System.out.println(number3);
-            System.out.println(number4);
 
             if (number1 != number2 && number1 != number3 && number1 != number4 && number2 != number3 && number2 != number4 && number3 != number4){
                 System.out.println("  верно  " );
