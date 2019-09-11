fd75b7515cd7d00b7a87cb7aede29fbadea46541
Kychinskaya <zhenya_247@mail.ru>
2019-09-11 17:12:57 +0300

    DZ - 18

---
+++ HM-18/src/StringExample.java
@@ -0,0 +1,18 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String str = new String("\"Странно!\"- громко воскликнул я. Вокруг меня было перевёрнуто всё : парты, стулья и школьная доска." +
+                                        " Неужели первоклассникам хватило перемены для этой революции?" +
+                                        " Я - уважаемый учитель; вокруг наконец-то воцарилась тишина...");
+        char symbol;
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            symbol = str.charAt(i);
+            if(symbol == '"' || symbol == '-' || symbol == '.' || symbol == ':' || symbol == ',' || symbol == '?' || symbol == '?' ){
+                    count++;
+            }
+
+        }
+        System.out.println("количество знаков препинания  = " + count);
+
+    }
+}
