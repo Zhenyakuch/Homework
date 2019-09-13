2c07d5fe3e488d4db677e68c5f6f8a379487ba58
Kychinskaya <zhenya_247@mail.ru>
2019-09-13 10:45:43 +0300

    DZ 22

---
+++ HM-22/src/Pattern_Matcher.java
@@ -0,0 +1,29 @@
+import javax.crypto.spec.PSource;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * Написать программу, выполняющую поиск в строке шестнадцатеричных чисел,
+ * записаных по правилам Java, с помощью регулярных выражений и вывести их на
+ * страницу.
+ */
+
+public class Pattern_Matcher {
+    public static void main(String[] args) {
+
+        String str = "0x7FFF мама  0x7AAF мыла      раму";
+        Pattern p = Pattern.compile("0x[A-Fa-f0-9]{1,4}");
+        Matcher m = p.matcher(str);
+        for (int i = 0; i < str.length() ; i++) {
+            while (m.find()) {
+                System.out.println(m.start() + " " + m.end());
+            }
+        }
+
+
+
+    }
+}
+
+
+
