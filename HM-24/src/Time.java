92f42abf0127b11421be5f8617b5bad0db62237d
Kychinskaya <zhenya_247@mail.ru>
2019-09-13 14:27:00 +0300

    DZ-24

---
+++ HM-24/src/Time.java
@@ -0,0 +1,56 @@
+/*Создать класс и объекты описывающие промежуток времени. Сам промежуток
+в классе должен задаваться тремя свойствами: секундами, минутами, часами.
+Сделать методы для получения полного количества секунд в объекте, сравнения
+двух объектов (метод должен работать аналогично compareTo в строках). Создать
+два конструктора: получающий общее количество секунд, и часы, минуты и секунды
+по отдельности. Сделать метод для вывода данных.*/
+
+public class Time {
+    private int second;
+    private long minute;
+    private  int hour;
+
+    public Time(int second, int hour){
+        this.second = second;
+        this.hour = hour;
+
+    }
+    public Time( int second, long minute){
+        this.second = second;
+        this.minute = minute;
+
+    }
+     public int Seconds(){
+        second = second + hour*60*60;
+        second = second + (int)minute*60;
+         System.out.println("Общее количество секунд объекта  " + second);
+         return second;
+     }
+
+    public void CompareTo(Time time2){
+        Time time1 = new Time(this.second, this.hour);
+        int sec1 = time1.Seconds();
+        int sec2 = time2.Seconds();
+
+        if(sec1>sec2){
+            System.out.printf("sec1 > sec2  ");
+            System.out.println(sec1 > sec2);
+
+        }else{
+            System.out.printf("sec1 < sec2  ");
+            System.out.println(sec1 < sec2);
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) { //add comment
+         Time time1 = new Time(49, 4);
+         Time time2 = new Time(55, 240L);
+         time1.CompareTo(time2);
+
+
+    }
+}
